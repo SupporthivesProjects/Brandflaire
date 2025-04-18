afad59913d3c9cdbe8580ede67724dca859d64f5
Vijay <gadgetzone@gadgetzones-MacBook-Air-2.local>
2025-04-18 17:25:30 +0530

    header footer and error file added and modifiy css

--- assest/js/main.js
+++ assest/js/main.js
@@ -7,6 +7,23 @@ $(window).scroll(function(){
         $('header').removeClass('color-header');
     }
 });
+// change src
+
+let countimg = 0;
+let toogleBtn = document.querySelector('.navbar-toggler');
+
+toogleBtn.addEventListener('click',()=>{
+    if(countimg == 0){
+      document.getElementById('changetoggle').src ="./assest/images/Menu-Button-cross.svg";
+      countimg = 1
+    }
+    else{
+        document.getElementById('changetoggle').src ="./assest/images/Menu-Button.svg";
+        countimg = 0;
+    }
+})
+
+
 AOS.init({
     duration: 1200,
 })
